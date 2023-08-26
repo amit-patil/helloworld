7530765a3e3343d1019d9a92a9d7e7ba3edb0f99
Amit Patil <amitpatil@MacBook-Pro-von-Amit.fritz.box>
2023-08-26 14:02:31 +0200

    GCP Test Code

---
+++ src/main/java/com/example/helloworld/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.example.helloworld;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class HelloWorldController {
+    @Value("${helloworld.message}")
+    public String message;
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> getHello(){
+        log.info("Called Hello World Control");
+        return ResponseEntity.ok().body("Google Cloud Platform says " + message);
+    }
+}
